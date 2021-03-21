683d4cf484f747c21d46c463206f8b94d89e0655
Rachel Padworski <rachelpadworski@gmail.com>
2021-03-20 22:53:05 -0600

    adds HTML and screenshot

--- header.php
+++ header.php
@@ -37,6 +37,31 @@
 <div class="wrap">
 
 <!-- begin header -->
-<header>
-	<!-- Add navigation menu, etc. -->
+<header class="site-header" role="banner">
+	<div class="container">
+
+		<div class="site-menu">
+			<a href="http://managed-it-solutions.local/" class="logo">
+				<img src="http://managed-it-solutions.local/wp-content/uploads/2021/03/logo.png" alt="logo" class="logo">
+			</a>
+			<i class="logo"><?php  ?></i>
+			<nav class="mobile-navigation">
+				<ul>
+					<li><a href="#services">OUR SERVICES</a></li>
+					<li><a href="#contractvsnoncontract">CONTRACT VS NON-CONTRACT</a></li>
+					<li><a href="#whymanagedit">WHY MANAGED IT?</a></li>
+					<li><a href="#contact">CONTACT US</a></li>
+				</ul>
+			</nav>
+			<div class="remote-support-btn">
+				<button id="remote-support-btn">Remote Support</button>
+			</div>
+		</div>
+
+		<h1 class="headline">Empower your business, and IT.</h1>
+		<h3 class="sub-headline">Computer IT services for commercial and residential clients in the greater Indianapolis area.</h3>
+		<button class="banner-btn">Contact Us</button>
+	
+	</div>
+
 </header>
