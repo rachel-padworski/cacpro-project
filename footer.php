2784e7be4521629ebec0ad773f06533c913c7650
Rachel Padworski <rachelpadworski@gmail.com>
2021-03-20 23:37:20 -0600

    adds footer

--- footer.php
+++ footer.php
@@ -3,8 +3,15 @@
 
 <!-- begin footer -->
 
-<footer>
-
+<footer class="site-footer">
+	<div class="footer-container">
+		<div class="site-footer__col-one">
+			<p>© 2020 Managed IT Solutions</p>
+			<i class="facebook-icon"> </i>
+			<p>Handcrafted by</p>
+			<img src="http://managed-it-solutions.local/wp-content/uploads/2021/03/cacpro.png" alt="cacpro-logo">
+		</div>
+	</div>
 </footer>
 <?php wp_footer(); ?>
 </body>
